0dc1aa790019dba6a36b1a905c04b8917ea27aec
root <root@DESKTOP-NSJQTUF>
2023-12-09 01:21:22 +0900

    add another loss function

--- show_result.py
+++ show_result.py
@@ -11,25 +11,27 @@ from model.dcrnn_supervisor import DCRNNSupervisor
 
 def run_dcrnn(args):
     with open(args.config_filename) as f:
-        b = np.load('data/dcrnn_predictions.npz')
-        print(b['prediction'][0][0].shape)
-        plt.plot(b['prediction'][0][0])
-        plt.plot(b['truth'][0][0])
-        plt.show()
+        # b = np.load('data/dcrnn_predictions.npz')
+        # print(b['prediction'][0][0].shape)
+        # plt.plot(b['prediction'][0][0], label="prediction")
+        # plt.plot(b['truth'][0][0], label="ground truth")
+        # plt.legend()
+        # plt.show()
 
-        return
+        # return
         supervisor_config = yaml.safe_load(f)
         
-
         graph_filename = supervisor_config[args.module].get('data').get('adj_csv_file')
         data_filename =  supervisor_config[args.module].get('data').get('csv_file')
         adj_mx = get_adjacency_matrix(graph_filename, data_filename)
 
         supervisor = DCRNNSupervisor(adj_mx=adj_mx, **supervisor_config.get(args.module))
-        mean_score, outputs = supervisor.evaluate('test')
-        print(outputs)
+        mean_score, mean_mape_score, mean_rmse_loss,  outputs = supervisor.evaluate('test')
         np.savez_compressed(args.output_filename, **outputs)
         print("MAE : {}".format(mean_score))
+        print("MAPE : {}".format(mean_mape_score))
+        print("RMSE : {}".format(mean_rmse_loss))
+
         print('Predictions saved as {}.'.format(args.output_filename))
 
 
